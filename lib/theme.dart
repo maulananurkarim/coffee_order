304ecf889a2bf2fba146659aed929c82d71e6cc5
Maulana Nurkarim <maulananurkarim.official@gmail.com>
2021-05-01 21:09:02 +0700

    commit

---
+++ lib/theme.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+Color backgroundColor = Color(0xffD1E1E0);
+Color greenColor = Color(0xff023E4A);
+Color greyColor = Color(0xff687A79);
+
+TextStyle titleTextStyle = TextStyle(
+  color: greenColor,
+  fontWeight: FontWeight.bold,
+  fontSize: 24,
+);
+
+TextStyle subtitleTextStlye = TextStyle(
+  color: greyColor,
+  fontWeight: FontWeight.w500,
+  fontSize: 12,
+);
+
+TextStyle blackTextStlye = TextStyle(
+  color: Colors.black,
+  fontWeight: FontWeight.w600,
+  fontSize: 12,
+);
